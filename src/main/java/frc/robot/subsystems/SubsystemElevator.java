1b029741de0933da942ee580355b6875ad97da93
Atx241 <atticusstewart241@gmail.com>
2025-01-09 16:52:18 -0700

    Removed a dependency from SubsystemElevator

--- src/main/java/frc/robot/subsystems/SubsystemElevator.java
+++ src/main/java/frc/robot/subsystems/SubsystemElevator.java
@@ -4,7 +4,7 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.controller.PIDController;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class SubsystemElevator extends SubsystemBase {
 
